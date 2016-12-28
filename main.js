38252f36ec2aef413d1ce45bd9fd669882ead92e
lilongllong <2453085960@qq.com>
2016-12-28 16:50:14 +0800

    Add electron.

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createMainBroserWindow()
+{
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 800,
+        center: true,
+        fullscreenable: false,
+        show: process.env.NODE_ENV === "development",
+        useCentersize: true
+    });
+    mainWindow.loadURL(`file://${__dirname}/dist/netease-music.html`);
+    mainWindow.on("closed", function() {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createMainBroserWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
